401d7cc9c449421e764cdd7b2f0e929417c67fbf
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-07-18 16:34:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,3 +58,40 @@ def position_taken?(board, index)
    turn_count(board) %2 == 0 ? player = "X" : player = "O"
    player
  end
+ require 'pry'
+ # Helper Method
+ def position_taken?(board, index)
+   !(board[index].nil? || board[index] == " ")
+ end
+
+ # Define your WIN_COMBINATIONS constant
+
+ WIN_COMBINATIONS = [
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [2,4,6]
+ ]
+
+
+ def won?(board)
+   WIN_COMBINATIONS.each do |win_move|
+   win_index_1 = win_move[0]
+   win_index_2 = win_move[1]
+   win_index_3 = win_move[2]
+
+   position_1 = board[win_index_1]
+   position_2 = board[win_index_2]
+   position_3 = board[win_index_3]
+
+   if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+     return win_move
+   end
+   end
+ else
+   false
+ end
