eab57d3185f731da86bdeb9cd72b425e3551695c
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-07-18 16:30:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,3 +21,14 @@ def position_taken? (board, index)
      return true
    end
 end
+def valid_move?(board, index)
+   index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+     return false
+   elsif board[index] == "X" || board[index] == "O"
+     return true
+   end
+ end
