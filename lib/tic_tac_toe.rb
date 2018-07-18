d1057215914db4ff1f4651a256e7b25224bc1d4f
Kelly Rae Ramirez <k.rae1993@yahoo.com>
2018-07-18 16:32:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,3 +32,15 @@ def position_taken?(board, index)
      return true
    end
  end
+ def turn(board)
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+   index = input_to_index(user_input)
+   if valid_move?(board, index)
+     move(board, index, "X")
+     display_board(board)
+   else
+     turn(board)
+   end
+ 
+ end
